ec1db4284e57867cef2bbcf19f082bcc04ee9388
mikhailnersesov <80050139+mikhailnersesov@users.noreply.github.com>
2021-05-04 22:12:09 +0200

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,8 +1,8 @@
 public class MainClass {
     public static int actual = 14;
 
-    public static void getLocalNumber() {
-        System.out.println(actual);
+    public static int getLocalNumber() {
+        return actual;
     }
 
     public static void main(String[] args) {
