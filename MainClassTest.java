a6bcf0706474ea1ee372a419cc9e864837f74d15
mikhailnersesov <80050139+mikhailnersesov@users.noreply.github.com>
2021-05-04 22:07:06 +0200

    Update MainClassTest.java

--- MainClassTest.java
+++ MainClassTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class MainClassTest extends MainClass {
     @Test
     public void testGetLocalNumber() {
-        int expected = 15;
+        int expected = 14;
         Assert.assertTrue("Not equal to 14", actual == expected);
     }
 }
