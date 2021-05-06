1b2b3442d29b959fced33c45edc18650849ed5cd
mikhailnersesov <80050139+mikhailnersesov@users.noreply.github.com>
2021-05-06 11:12:42 +0200

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,11 +1,12 @@
 public class MainClass {
-    private static int class_number = 20;
 
-    public static int getClassNumber() {
-        return class_number;
+    private static String class_string = "Hello, world";
+
+    public static String getClassString() {
+        return class_string;
     }
 
     public static void main(String[] args) {
-        getClassNumber();
+        getClassString();
     }
 }
