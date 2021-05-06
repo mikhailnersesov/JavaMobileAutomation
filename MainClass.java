16eacee9f7f5c4b5b54c5e13ca55add158a42ab6
mikhailnersesov <80050139+mikhailnersesov@users.noreply.github.com>
2021-05-06 11:06:05 +0200

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+public class MainClass {
+
+    private static String class_string = "Hello, world";
+
+    public static String getClassString() {
+        return class_string;
+    }
+
+    public static void main(String[] args) {
+        getClassString();
+    }
+}
