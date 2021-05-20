78129fef9d4321d4f7d8b4c0ae3ed7c24dc356db
mikhailnersesov <80050139+mikhailnersesov@users.noreply.github.com>
2021-05-20 12:00:30 +0200

    Update MainClassTest.java

--- MainClassTest.java
+++ MainClassTest.java
@@ -6,19 +6,11 @@ public class MainClassTest extends MainClass{
     public void testGetClassString(){
 
         String expected = "Hello";
-        Assert.assertTrue("Does not match Hello or hello",getClassString().toLowerCase().contains(expected.toLowerCase()));
+        String expected2 = "hello";
+        Assert.assertTrue(
+                "Does not match Hello or hello",
+                getClassString().contains(expected)  || getClassString().contains(expected2)
+        );
 
-        /*
-        my tryouts, interesting to look at the end of the course:
-
-        String b = getClassString().toLowerCase();
-        System.out.println(b);
-
-        Assert.assertTrue("Does not match Hello or hello","hello, world".equalsIgnoreCase(getClassString()));
-
-        Assert.assertTrue(boolean containsIgnoreCase("hello, world", getClassString()));
-        String toLowerCase("Hello");
-        Assert.assertTrue("Does not match Hello or hello",getClassString().contains(expected));
-         */
     }
 }
